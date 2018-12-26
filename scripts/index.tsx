9e6a13759e73ba885c07c588afe15c950776e918
Zerg00s <denis-ne@yandex.ru>
2018-12-26 13:53:45 -0500

    cleaned index.tsx

--- scripts/index.tsx
+++ scripts/index.tsx
@@ -1,10 +1,5 @@
-// Here goes your awesome TypeScript
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import * as Subtitle from 'subtitle'
 import { Subtitles } from './components/Subtitles'
 
-
-ReactDOM.render(
-    <Subtitles />
-    , document.getElementById('root'));
+ReactDOM.render(<Subtitles /> , document.getElementById('root'));
